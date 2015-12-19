6d7784fefb9cf5cee3402fc5df3513359e31fa0b
Coderrob <rob@coderrob.com>
2015-12-18 20:53:44 -0500

    Advent Day11 puzzle challenge. Still working to determine proper password generation.

---
+++ day11/helper.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"strings"
+)
+
+// PasswordLengthRequirement is the required length of the Elf security password
+const PasswordLengthRequirement = 8
+
+var forbiddenCharacters = []byte { 'i', 'o', 'l'}
+
+func getsWhetherPasswordMeetsSecurityPolicy(input string) bool {
+	if meetsRequirement := meetsSecurityLengthRequirement(input); !meetsRequirement {
+		return false
+	}	
+	if hasOnlyLowercaseCharacters := containsOnlyLowercaseCharacters(input); !hasOnlyLowercaseCharacters {
+		return false
+	}	
+	if hasForbiddenCharacters := containsAnyForbiddenCharacters(input); hasForbiddenCharacters {
+		return false
+	}	
+	if hasTwoPairsOfCharacters := containsTwoPairsOfUniqueCharacters(input); !hasTwoPairsOfCharacters {
+		return false
+	}
+	if hasStraightOfCharacters := containsIncreasingStraightOfCharacters(input); !hasStraightOfCharacters {
+		return false
+	}
+	return true
+}
+
+func containsOnlyLowercaseCharacters(input string) bool {
+	for index := 0; index < len(input); index++ {
+		var character = input[index]
+		if isLowercaseASCIICharacter(character) == false {
+			return false
+		}
+	}
+	return true
+}
+
+func isLowercaseASCIICharacter(character byte) bool {
+	return character >= 'a' && character <= 'z'
+}
+
+func meetsSecurityLengthRequirement(input string) bool {
+	return len(input) == PasswordLengthRequirement 
+}
+
+func containsAnyForbiddenCharacters(input string) bool {
+	for _, forbiddenCharacter := range forbiddenCharacters {
+		if strings.ContainsAny(input, string(forbiddenCharacter)) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsIncreasingStraightOfCharacters(input string) bool {
+	inputLength := len(input)
+	for index := 0; index < inputLength; index++ {
+		if index + 2 < inputLength {
+			var character = input[index]
+			if (character + 1) == input[index + 1] && (character + 2) == input[index + 2] {				
+				return true
+			}
+			index = index + 2
+		}
+	}
+	return false
+}
+
+func containsTwoPairsOfUniqueCharacters(input string) bool {
+	var pairOfCharacterCount int
+	var firstPairsCharacter byte
+	for index := 0; index < len(input); index++ {
+		var character = input[index]
+		if strings.Contains(input, string(character) + string(character)) {
+			if pairOfCharacterCount == 0 {
+				firstPairsCharacter = character
+				pairOfCharacterCount++
+			} else if firstPairsCharacter != character {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ day11/tests.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func runPasswordValidationTests() {
+	fmt.Println(getsWhetherPasswordMeetsSecurityPolicy("hijklmmn") == false)
+	fmt.Println(getsWhetherPasswordMeetsSecurityPolicy("abbceffg") == false)
+	fmt.Println(getsWhetherPasswordMeetsSecurityPolicy("abbcegjk") == false)
+
+	fmt.Println(meetsSecurityLengthRequirement("a") == false)
+	fmt.Println(meetsSecurityLengthRequirement("1") == false)
+	fmt.Println(meetsSecurityLengthRequirement("aaaaaaaa") == true)
+
+	fmt.Println(isLowercaseASCIICharacter('0') == false)
+	fmt.Println(isLowercaseASCIICharacter('z' + 1) == false)
+	fmt.Println(isLowercaseASCIICharacter('a') == true)
+	fmt.Println(isLowercaseASCIICharacter('z') == true)	
+	
+	fmt.Println(containsOnlyLowercaseCharacters("A") == false)
+	fmt.Println(containsOnlyLowercaseCharacters("a") == true)
+
+	fmt.Println(containsAnyForbiddenCharacters("abcd") == false)
+	fmt.Println(containsAnyForbiddenCharacters("abcdi") == true)
+	fmt.Println(containsAnyForbiddenCharacters("abocd") == true)
+	fmt.Println(containsAnyForbiddenCharacters("albcd") == true)
+	
+	fmt.Println(containsTwoPairsOfUniqueCharacters("abccd") == false)
+	fmt.Println(containsTwoPairsOfUniqueCharacters("aabccd") == true)
+
+	fmt.Println(containsIncreasingStraightOfCharacters("abecd") == false)
+	fmt.Println(containsIncreasingStraightOfCharacters("abccd") == true)
+
+	fmt.Println(generateNewPassword("abcdefgh")) // abcdffaa
+	fmt.Println(generateNewPassword("ghijklmn")) // ghjaabcc
+}
